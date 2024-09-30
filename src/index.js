ca9fb7df2c04e39537a2b66a078c83ea25eaccf6
Anastasia18432 <zastupanastasia7@gmail.com>
2024-09-30 09:05:58 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
 
 import "./style/style.scss";
-
-const marvelService = new MarvelService();
-
-marvelService
-  .getCharacter(1011334)
-  .then((res) => res.data.results.forEach((item) => console.log(item.name)))
-  .catch((error) => console.error(error));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
