c345370dc7baab6afbdd9dc5cac3ba4b68893dfd
Anastasia18432 <zastupanastasia7@gmail.com>
2024-11-17 23:56:32 +0200

    add

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "./../appBanner/AppBanner";
 
 import decoration from "../../resources/img/vision.png";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   const [selectedChar, setChar] = useState(null);
@@ -16,25 +19,29 @@ const App = () => {
   };
 
   return (
-    <div className="app">
-      <AppHeader />
-      <main>
-        <ErrorBoundary>
-          <RandomChar />
-        </ErrorBoundary>
-        <div className="char__content">
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
           <ErrorBoundary>
-            <CharList onCharSelected={onCharSelected} />
+            <RandomChar />
           </ErrorBoundary>
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar} />
-          </ErrorBoundary>
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" />
-
-        <ComicsList />
-      </main>
-    </div>
+          <div className="char__content">
+            <ErrorBoundary>
+              <CharList onCharSelected={onCharSelected} />
+            </ErrorBoundary>
+            <ErrorBoundary>
+              <CharInfo charId={selectedChar} />
+            </ErrorBoundary>
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision" />
+          <Route>
+            <AppBanner />
+            <ComicsList />
+          </Route>
+        </main>
+      </div>
+    </Router>
   );
 };
 
